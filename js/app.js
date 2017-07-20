fe7cc411862bede557c7fe3bf42eb47e00f4ff22
Karlyn Kaai <karlynkaai@Karlyns-MacBook-Pro.local>
2017-07-19 20:50:26 -1000

    completed assignment Price favorite song

--- js/app.js
+++ js/app.js
@@ -4,7 +4,10 @@
 
 Find the div with the id of "name1" and replace the n/a with the following:
 
+
 Tay-Tay*/
+var newName = document.getElementById("name1");
+newName.innerHTML= "Tay Tay";
 
 
 /*2. DJ Khaled
@@ -12,6 +15,8 @@ Tay-Tay*/
 Find the div with the id of "position2" and replace the n/a with the following: 
 
 Project Manager*/
+var khaledPosition = document.getElementById("position2");
+khaledPosition.innerHTML = "Project Manager";
 
 
 /*3. Piko Taro
@@ -20,12 +25,18 @@ Find the div with the id of "alias3" and replace the n/a with the following:
 
 Concatenation*/
 
-
+var pikoChange = document.getElementById("alias3");
+pikoChange.innerHTML = "Concatenation";
 
 
 /*4. Prince
 
 Find the div with the class name of "profile" and replace the n/a with a verse from your favorite Prince song.*/
+
+var divElem = document.getElementsByClassName("profile")
+console.log(divElem);
+divElem[0].innerHTML = "Purple rain, purple rain";
+
 
 
 
